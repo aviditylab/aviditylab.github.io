919b8600a8a980491e715e168e6d20b9ea6141c4
albert randi <albertrandi72@gmail.com>
2024-05-20 22:18:05 +0700

    update style

--- src/components/Faq/FaqItem/index.tsx
+++ src/components/Faq/FaqItem/index.tsx
@@ -3,10 +3,10 @@ import { FaMinus, FaPlus } from "react-icons/fa";
 
 interface FaqItemProps {
   question: string
-  answer: string
+  answers: string[]
 }
 
-const FaqItem: React.FC<FaqItemProps> = ({ question, answer }) => {
+const FaqItem: React.FC<FaqItemProps> = ({ question, answers }) => {
   const [show, setShow] = useState(false);
   return (<div className=" border-b-2 border-solid border-gray-700 py-3 ">
     <div className=" flex justify-between  items-center" onClick={() => setShow((prev) => !prev)}>
@@ -22,8 +22,8 @@ const FaqItem: React.FC<FaqItemProps> = ({ question, answer }) => {
         
       </div>
     </div>
-      <div className={`transition duration-150 ease-out hover:ease-in font-satoshi text-xs text-gray-500 overflow-hidden ${!show ? "h-0" : ""}`}>
-        {answer}
+      <div className={` flex flex-col space-y-4 transition duration-150 ease-out hover:ease-in font-satoshi text-xs text-gray-500 overflow-hidden ${!show ? "h-0" : ""}`}>
+        {answers.map(ans => (<div>{ans}</div>))}
       </div>
   </div>)
 }
