4c174df57098b36861969e08bd16f3cf97db7e19
albertrandi72 <albertrandi72@gmail.com>
2024-06-26 13:43:35 +0700

    Updates

---
+++ ebc70433-b8f2eaf2c49f9a260194.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkavidity_lab=self.webpackChunkavidity_lab||[]).push([[958],{6512:function(t,a,i){i.d(a,{WQq:function(){return u}});var n=i(2225);function u(t){return(0,n.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=ebc70433-b8f2eaf2c49f9a260194.js.map
