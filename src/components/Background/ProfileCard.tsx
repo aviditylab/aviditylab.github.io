ed137306bd04184f5ef1c66471c8144e7c3d03e0
albert randi <albertrandi72@gmail.com>
2024-09-13 16:52:39 +0700

    update

---
+++ src/components/Background/ProfileCard.tsx
@@ -0,0 +1,9 @@
+export default function () {
+  return (
+    <div className="p-4 bg-[#343A41]">
+      <div className=" px-6 py-2 bg-[#4169E2] text-white cursor-pointer">
+        Submit
+      </div>
+    </div>
+  )
+}
